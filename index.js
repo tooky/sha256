25772fc1635eb96e0ded446d7080d45c81039f93
Steve Tooke <steve@took.es>
2023-09-13 09:14:44 +0100

    Initial app

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { createHash } from 'node:crypto'
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+   console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/status", (_req, res) => {
+   const status = {
+      "Status": "Running"
+   }
+
+   res.send(status)
+})
+
+app.post('/data', (req, res) => {
+   const payload = req.body['payload']
+   const digest = createHash('sha256').update(payload).digest('hex')
+   const result = {
+      digest
+   }
+
+   res.send(result)
+})
